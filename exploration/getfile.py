50d301ab90c26cbc3c57fa346e2acd3d18697b0c
Arnav Borborah <arnavborborah11@gmail.com>
2023-05-17 16:28:56 -0400

    Slightly refactor requests call to use params

--- exploration/getfile.py
+++ exploration/getfile.py
@@ -1,13 +1,11 @@
 import requests
 
 def getfile(year=2023, term=1, campus='NB'):
-    url = "https://sis.rutgers.edu/soc/api/courses.json?"
-    url += 'year=' + str(year) + '&'
-    url += 'term=' + str(term) + '&'
-    url += 'campus=' + campus
+    url = "https://sis.rutgers.edu/soc/api/courses.json"
 
-    result = requests.get(url).json()
-    return result
+    response = requests.get(url, params={"year": year, "term": term, "campus": campus})
+    
+    return response.json()
 
 if __name__ == "__main__":
     result = getfile()
