07aa4a9c412cac65cfafebf75f48ce7cca673b80
Jayden Barnes <11010206+resigned@users.noreply.github.com>
2023-05-26 21:35:50 -0400

    database

--- src/api/db.ts
+++ src/api/db.ts
@@ -7,12 +7,18 @@ dotenv.config();
 const dbUrl = process.env.DB_URL;
 const dbName = process.env.DB_NAME;
 
+logger.info("Attempting to connect to database");
+
 mongoose
   .connect(dbUrl || "", {
     dbName: dbName || ""
   })
   .then(() => logger.info("Connected to database"))
   .catch((err) => logger.error(err));
+
+mongoose.connection.on("error", (err) => {
+  logger.error(err);
+});
 
 process.on("exit", function () {
   mongoose.disconnect();
