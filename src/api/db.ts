53b451f34100388d12b8ae59b166b0397899cee2
Jayden Barnes <11010206+resigned@users.noreply.github.com>
2023-05-26 21:03:00 -0400

    added dbName environment variable

--- src/api/db.ts
+++ src/api/db.ts
@@ -5,10 +5,11 @@ import { logger } from "../logger";
 dotenv.config();
 
 const dbUrl = process.env.DB_URL;
+const dbName = process.env.DB_NAME;
 
 mongoose
   .connect(dbUrl || "", {
-    dbName: "RCV-test"
+    dbName: dbName || ""
   })
   .then(() => logger.info("Connected to database"))
   .catch((err) => logger.error(err));
