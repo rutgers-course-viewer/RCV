b38109e203fc44121c407717b56655b85ca50d5c
Arnav Borborah <arnavborborah11@gmail.com>
2023-08-21 11:17:03 -0400

    ROADM-7: Add custom colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			// Rutgers red
+			primary: '#CA0037',
+			// True gray from Figma extended tailwind color palette
+			gray: {
+				10: '#F5F5F5',
+				50: '#FAFAFA',
+				200: '#E5E5E5',
+				300: '#D4D4D4',
+				400: '#A3A3A3',
+				500: '#737373',
+				600: '#525252',
+				700: '#404040',
+				800: '#262626',
+				900: '#262626'
+			}
+		},
 		extend: {}
 	},
 	plugins: []
