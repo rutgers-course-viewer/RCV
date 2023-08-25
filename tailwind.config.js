07b38c2caddfe6e8b03f4be4148efadd1fdf3ae3
Arnav Borborah <arnavborborah11@gmail.com>
2023-08-25 17:50:52 -0400

    ROADM-7: Temporarily comment out colors until the right set is chosen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}', 'node_modules/preline/dist/*.js'],
-	theme: {
-		colors: {
-			// Rutgers red
-			primary: '#CA0037',
-			// True gray from Figma extended tailwind color palette
-			gray: {
-				10: '#F5F5F5',
-				50: '#FAFAFA',
-				200: '#E5E5E5',
-				300: '#D4D4D4',
-				400: '#A3A3A3',
-				500: '#737373',
-				600: '#525252',
-				700: '#404040',
-				800: '#262626',
-				900: '#262626'
-			}
-		},
-		extend: {}
-	},
+	// Temporarily comment out until we find right set of colors
+	// theme: {
+	// 	colors: {
+	// 		// Rutgers red
+	// 		primary: '#CA0037',
+	// 		// True gray from Figma extended tailwind color palette
+	// 		gray: {
+	// 			10: '#F5F5F5',
+	// 			50: '#FAFAFA',
+	// 			200: '#E5E5E5',
+	// 			300: '#D4D4D4',
+	// 			400: '#A3A3A3',
+	// 			500: '#737373',
+	// 			600: '#525252',
+	// 			700: '#404040',
+	// 			800: '#262626',
+	// 			900: '#171717'
+	// 		}
+	// 	},
+	// 	extend: {}
+	// },
 	plugins: [require('preline/plugin')]
 };
