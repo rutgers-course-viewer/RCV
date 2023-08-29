190c3f83044fc73cb84e8ac61c9e8a7d57b5521a
Jayden Barnes <11010206+resigned@users.noreply.github.com>
2023-08-29 01:36:04 -0400

    change title for calendar

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -45,7 +45,7 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Week Calendar in TailwindCSS</title>
+		<title>Week Calendar Demo - RCV</title>
 	</head>
 
 	<body>
